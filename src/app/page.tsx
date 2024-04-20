49f8e2eac1dcfd9204c34ca45002cdabb5e35efc
Emilio Andere <andere@uchicago.edu>
2024-04-20 16:19:42 -0500

    inspiration banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'; 
 
 import { PodcastCard } from '@/components/podcast-card'
+import { RandomIcon } from '@/components/random-icon';
 import { Result, Search } from '@/components/search'
 import { useState } from 'react'
 
@@ -22,7 +23,10 @@ export default function Home() {
           {results ? results.filter(result => result.title !== "Spotify Embed").map((result) => (
             <PodcastCard key={result.id} {...result} />
           )) : (
-            <div className="col-span-5 text-center text-gray-500"></div>
+            <div className='col-span-5 row-span-10 text-center text-gray-500"'>
+              <p className='text-xl'><span className='font-bold'>Tip</span>: Click on <RandomIcon fill='gray' className='inline-block align-middle' height='1.5em' /> for inspiration!</p>
+
+            </div>
           )}
         </div>
       </main>
