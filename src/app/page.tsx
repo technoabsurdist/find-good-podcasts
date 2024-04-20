5aa6da0dd3875c9472a26430e9b70bc7a84e83c8
Emilio Andere <andere@uchicago.edu>
2024-04-20 16:26:14 -0500

    slight background color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,8 @@ export default function Home() {
           {results ? results.filter(result => result.title !== "Spotify Embed").map((result) => (
             <PodcastCard key={result.id} {...result} />
           )) : (
-            <div className='col-span-5 row-span-10 text-center text-gray-500"'>
+            <div className='col-span-5 row-span-10 text-center text-gray-600'>
               <p className='text-xl'><span className='font-bold'>Tip</span>: Click on <RandomIcon fill='gray' className='inline-block align-middle' height='1.5em' /> for inspiration!</p>
-
             </div>
           )}
         </div>
